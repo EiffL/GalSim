44abbc0ae2b64f593e132fdde67460c80b72c9f4
EiffL <fr.eiffel@gmail.com>
2017-03-25 22:39:02 -0400

    Added base class for generative model

---
+++ galsim/generative_model.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2012-2017 by the GalSim developers team on GitHub
+# https://github.com/GalSim-developers
+#
+# This file is part of GalSim: The modular galaxy image simulation toolkit.
+# https://github.com/GalSim-developers/GalSim
+#
+# GalSim is free software: redistribution and use in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions, and the disclaimer given in the accompanying LICENSE
+#    file.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions, and the disclaimer given in the documentation
+#    and/or other materials provided with the distribution.
+#
+"""@file generative_model.py
+Base class defining a generative model of galaxy images.
+
+A generative model can be trained on some real astrophysical data and then used
+to conditionally sample new galaxy images. 
+
+"""
+
